862dc385a9e62dd2a24c0ae3e007e2dc68074f4c
Nurman Hadi <nurman.hadi@hotmail.com>
2024-12-12 13:17:22 +0700

    validation

--- core/application/dtos/order.dto.go
+++ core/application/dtos/order.dto.go
@@ -6,12 +6,12 @@ type AddRequestOrder struct {
 	PickupDate      string `json:"pickup_date" validate:"required"`
 	DropOffDate     string `json:"dropoff_date" validate:"required"`
 	PickupLocation  string `json:"pickup_location" validate:"required,min=1,max=50"`
-	DropOffLocation string `json:"dropoff_location" validate:"required,min=1,max50"`
+	DropOffLocation string `json:"dropoff_location" validate:"required,min=1,max=50"`
 }
 type UpdateRequestOrder struct {
 	OrderDate       *string `json:"order_date" validate:"omitempty"`
 	PickupDate      *string `json:"pickup_date" validate:"omitempty"`
 	DropOffDate     *string `json:"dropoff_date" validate:"omitempty"`
 	PickupLocation  *string `json:"pickup_location" validate:"omitempty,min=1,max=50"`
-	DropOffLocation *string `json:"dropoff_location" validate:"omitempty,min=1,max50"`
+	DropOffLocation *string `json:"dropoff_location" validate:"omitempty,min=1,max=50"`
 }
